b9fc800a5d1f6f20255dfdd2721d699dd3c69bea
LoyalCalf <1345285903@qq.com>
2018-04-02 13:05:27 +0800

    更新前端内容

--- static/js/lib/supersized-init.js
+++ static/js/lib/supersized-init.js
@@ -20,9 +20,9 @@ jQuery(function($){
         // 组件
         slide_links        : 'blank',    // 个别环节为每张幻灯片（选项：假的，'民'，'名'，'空'）
         slides             : [    // 幻灯片影像
-                                 {image : '../../static/images/1.jpg'},
-                                 {image : '../../static/images/2.jpg'},
-                                 {image : '../../static/images/3.jpg'}
+                                 {image : '../../static/images/login5.jpg'},
+                                 // {image : '../../static/images/2.jpg'},
+                                 // {image : '../../static/images/3.jpg'}
                        ]
 
     });
